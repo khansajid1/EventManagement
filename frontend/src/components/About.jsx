4524af95a13f56eaed8a10c0ecaceb2a94ff36f9
mohd sajid <130218017+khansajid1@users.noreply.github.com>
2024-12-19 22:38:10 +0530

    EventManagement-JavaScript

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+      Event management involves planning, organizing, and executing events such as conferences, weddings, concerts, or corporate gatherings. It encompasses all aspects, from budgeting and venue selection to coordinating vendors, managing logistics, and ensuring smooth execution. Event managers work closely with clients to understand their goals and deliver a memorable experience. Successful event management requires attention to detail, creativity, and strong communication skills to meet client expectations and create impactful, well-organized events.
+      </p>
+    </section>
+  );
+};
+
+export default About;
